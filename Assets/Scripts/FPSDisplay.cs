d4ea0218ea57b1ddd5d7368ae146e8265b50bcac
dobrMAN <chibiriak@gmail.com>
2017-04-21 02:23:17 +0800

    Переписал счетчик FPS без OnGUI

--- Assets/Scripts/FPSDisplay.cs
+++ Assets/Scripts/FPSDisplay.cs
@@ -1,28 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class FPSDisplay : MonoBehaviour
 {
     float deltaTime = 0.0f;
+    [SerializeField]
+    Text UItext;
 
     void Update()
     {
         deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-    }
-
-    void OnGUI()
-    {
-        int w = Screen.width, h = Screen.height;
-
-        GUIStyle style = new GUIStyle();
-
-        Rect rect = new Rect(0, 0, w, h * 3 / 100);
-        style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 3 / 100;
-        style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
         float msec = deltaTime * 1000.0f;
         float fps = 1.0f / deltaTime;
         string text = string.Format("{0:0.} fps ({1:0.0} ms)", fps, msec);
-        GUI.Label(rect, text, style);
+        UItext.text = text;
     }
+
+
 }
